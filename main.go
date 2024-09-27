72468d352b11c38a09096c60e7d471f90ab13e85
Your Name <you@example.com>
2024-09-27 02:58:58 +0100

    jhjhk

--- main.go
+++ main.go
@@ -20,15 +20,83 @@ type Vertix struct {
 //AddVertex  ajouter une noude au graph
 
 func (g *Graph) AddVertex(key int) {
+	if !Contain(g.Vertices,key) { 
 	g.Vertices = append( g.Vertices,&Vertix{nam:key})
+	} else {
+		err := fmt.Errorf("la noude %v est deja exist! ",key)
+		fmt.Println(err)
+	} 
+}
+func (G *Graph) Print() {
+	for _,v := range G.Vertices {
+		fmt.Print("\nvertice : %v ",v.nam)
+		for _,v := range v.adjacenlist {
+			fmt.Print(" %v ",v.nam)
+		}
+	}
 }
 
+// AddEdge add an edge to the graphe
+
+func (g *Graph) AddEdge (from,to int) {
+
+	// Get Vertex 
+	FromVertex := g.GetVertex(from)
+	ToVertex := g.GetVertex(to)
+
+	//check err 
+	if FromVertex == nil || ToVertex == nil {
+		err := fmt.Errorf("invalid Edge %v==>%v",from,to)
+		fmt.Println(err)
+	} else if Contain(FromVertex.adjacenlist,to) {
+		err := fmt.Errorf("%v==>%v  est deja exist!",from,to)
+		fmt.Println(err)
+
+
+	}else {
+		FromVertex.adjacenlist = append(FromVertex.adjacenlist, ToVertex)
+	}
+
+
+
+
+}
+func (G *Graph) GetVertex(k int) *Vertix {
+	for i,v  := range  G.Vertices {
+		if k == v.nam {
+
+			 return G.Vertices[i]
+
+		}
+	}
+	return nil
+
+}
+func Contain(s []*Vertix,key int)bool {
+
+	for _,v := range s {
+		if key == v.nam {
+			return true
+		}
+	}
+	return false 
+}
 func main () {
 	test:=&Graph{}
 	for i:=0 ; i < 5 ; i++ {
 		test.AddVertex(i)
 	}
-	fmt.Println(test)
+	test.AddEdge(1,3)
+	test.AddEdge(1,3)
+	test.AddEdge(1,3)
+	test.AddEdge(1,9)
+	test.AddEdge(1,96)
+	test.AddVertex(0)
+	test.AddVertex(1)
+	test.AddVertex(10)
+	test.AddVertex(3)
+	test.AddVertex(0)
+	test.Print()
 }
 
 
